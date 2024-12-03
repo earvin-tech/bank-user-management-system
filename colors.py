6862882b5bd4c483051d6cbf66d34a328b492309
earvin-tech <wsl@Earvin>
2024-12-03 12:44:42 +1100

    Comments on colors

--- colors.py
+++ colors.py
@@ -1,6 +1,6 @@
 from colored import Fore, Back, Style # type: ignore
 
-
+# Different colors to call for printing on console
 error_color: str = f"{Back.red}{Style.underline}{Style.bold}"
 reset: str = f"{Style.reset}"
 checking_account_color: str = f"{Back.blue}"
